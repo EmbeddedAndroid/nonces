a05da6168c5eedfbc174b1aba771442a29a14a47
Tyler Baker <tyler@opensourcefoundries.com>
2018-05-04 00:10:52 -0700

    Initial commit
    Signed-off-by: Tyler Baker <tyler@opensourcefoundries.com>

---
+++ js/config.js
@@ -0,0 +1,6 @@
+"use strict"
+var subscribeUrl = "http://octrace.us14.list-manage.com/subscribe/post?u=b65c95763e0ae8e0c14e7af0e&amp;id=7e9cf89638";
+var email = "octrace@gmail.com";
+var countdown = true;
+var configDate = "2018 05 13";
+var particles = true;
